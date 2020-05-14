69539baf53f6809266e90eeb902b9b411ca64ef2
fubira <fubira@gmail.com>
2020-05-14 15:32:24 +0900

    bundle.js

--- webpack/prod.js
+++ webpack/prod.js
@@ -6,7 +6,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 module.exports = merge(base, {
   mode: "production",
   output: {
-    filename: "bundle.min.js",
+    filename: "bundle.js",
   },
   devtool: false,
   performance: {
